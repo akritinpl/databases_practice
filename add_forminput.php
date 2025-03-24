b9a011048714a3f2325dc8ece71069bfa9b0092c
akritinpl <139466603+akritinpl@users.noreply.github.com>
2025-03-24 00:22:14 -0400

    First commit

---
+++ add_forminput.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Input</title>
+</head>
+<body>
+<form method="POST">
+Number 1: <input type="text"> name="num1"><br>
+Number 2: <input type="text"> name="num2"><br>
+<input type="submit">
+<?php
+The sum is: <?php echo $_POST["num1"] + $_POST["num2"]; ?>
+?>
+</form>
+</body>
+</html>
